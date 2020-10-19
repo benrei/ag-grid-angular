c5d73d9d59da4a689e5a7aafed1b6c2c6bc59e25
benrei <benrei.dev@gmail.com>
2020-10-19 15:24:20 +0200

    2 files modified, 1 added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,12 @@ import { FormsModule } from "@angular/forms";
 import { AppComponent } from "./app.component";
 import { HelloComponent } from "./hello.component";
 import { SidenavService } from "./sidenav.service";
-import { ToolbarComponent } from "./toolbar/toolbar.component";
 import { AllMaterialModules } from "./material/material.module";
+import { ToolbarComponent } from "./toolbar/toolbar.component";
 
 @NgModule({
-  imports: [BrowserModule, FormsModule, AllMaterialModules],
-  declarations: [AppComponent, HelloComponent, ToolbarComponent],
+  imports: [BrowserModule, FormsModule, AllMaterialModules, ToolbarComponent],
+  declarations: [AppComponent, HelloComponent],
   bootstrap: [AppComponent],
   providers: [SidenavService]
 })
