a6cfc9abee839f75309d27fc48a8e90765159508
benrei <benrei.dev@gmail.com>
2020-10-19 15:20:13 +0200

    4 files modified, 5 added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,17 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
-import { FormsModule } from '@angular/forms';
+import { NgModule } from "@angular/core";
+import { BrowserModule } from "@angular/platform-browser";
+import { FormsModule } from "@angular/forms";
 
-import { AppComponent } from './app.component';
-import { HelloComponent } from './hello.component';
+import { AppComponent } from "./app.component";
+import { HelloComponent } from "./hello.component";
+import { SidenavService } from "./sidenav.service";
+import { ToolbarComponent } from "./toolbar/toolbar.component";
+import { AllMaterialModules } from "./material/material.module";
 
 @NgModule({
-  imports:      [ BrowserModule, FormsModule ],
-  declarations: [ AppComponent, HelloComponent ],
-  bootstrap:    [ AppComponent ]
+  imports: [BrowserModule, FormsModule, AllMaterialModules],
+  declarations: [AppComponent, HelloComponent, ToolbarComponent],
+  bootstrap: [AppComponent],
+  providers: [SidenavService]
 })
-export class AppModule { }
+export class AppModule {}
---
+++ src/app/sidenav.service.ts
@@ -0,0 +1,8 @@
+import { Injectable } from "@angular/core";
+import { MatSidenav } from "@angular/material/sidenav";
+
+@Injectable()
+export class SidenavService {
+  public sideNav: MatSidenav;
+  constructor() {}
+}
