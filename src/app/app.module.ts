62c749423d7149ea875ae93e45b7f9443a2658db
benrei <benrei.dev@gmail.com>
2020-10-20 12:34:57 +0200

    2 files modified

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,9 +7,10 @@ import { AllMaterialModules } from "./material/material.module";
 import { ToolbarModule } from "./toolbar/toolbar.module";
 import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
 import { MenuComponent } from "./menu/menu.component";
-import { Routes, RouterModule } from "@angular/router/router";
+import { Routes, RouterModule } from "@angular/router";
 
 const appRoutes: Routes = [
+  { path: "", component: null },
   { path: "test", component: null },
   { path: "test2", component: null },
   { path: "test3", component: null }
