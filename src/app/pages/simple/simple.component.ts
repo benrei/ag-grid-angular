9779cdb01345abc97843a58aba8eb4dd75888fdc
benrei <benrei.dev@gmail.com>
2020-10-20 15:26:49 +0200

    5 files modified

--- src/app/pages/simple/simple.component.ts
+++ src/app/pages/simple/simple.component.ts
@@ -1,15 +1,70 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { HttpClient } from "@angular/common/http";
 
 @Component({
-  selector: 'app-simple',
-  templateUrl: './simple.component.html',
-  styleUrls: ['./simple.component.css']
+  selector: "app-simple",
+  templateUrl: "./simple.component.html",
+  styleUrls: ["./simple.component.css"]
 })
-export class SimpleComponent implements OnInit {
+export class SimpleComponent {
+  private gridApi;
+  private gridColumnApi;
 
-  constructor() { }
+  private columnDefs;
+  private defaultColDef;
+  private autoGroupColumnDef;
+  private rowData: [];
 
-  ngOnInit() {
+  constructor(private http: HttpClient) {
+    this.columnDefs = [
+      {
+        field: "country",
+        rowGroup: true,
+        hide: true
+      },
+      {
+        field: "year",
+        rowGroup: true,
+        hide: true
+      },
+      {
+        field: "sport",
+        minWidth: 200
+      },
+      {
+        field: "athlete",
+        minWidth: 200
+      },
+      { field: "gold" },
+      { field: "silver" },
+      { field: "bronze" },
+      { field: "total" },
+      { field: "age" },
+      {
+        field: "date",
+        minWidth: 140
+      }
+    ];
+    this.defaultColDef = {
+      flex: 1,
+      minWidth: 100,
+      filter: true,
+      sortable: true,
+      resizable: true
+    };
+    this.autoGroupColumnDef = { minWidth: 200 };
   }
 
+  onGridReady(params) {
+    this.gridApi = params.api;
+    this.gridColumnApi = params.columnApi;
+
+    this.http
+      .get(
+        "https://raw.githubusercontent.com/ag-grid/ag-grid/master/grid-packages/ag-grid-docs/src/olympicWinnersSmall.json"
+      )
+      .subscribe(data => {
+        this.rowData = data;
+      });
+  }
 }
