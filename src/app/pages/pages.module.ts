cd6dc9b6cf38518f300716e5d74c05a7d29c8e0e
benrei <benrei.dev@gmail.com>
2020-10-20 15:29:38 +0200

    4 files modified

--- src/app/pages/pages.module.ts
+++ src/app/pages/pages.module.ts
@@ -2,8 +2,11 @@ import { NgModule } from "@angular/core";
 import { CommonModule } from "@angular/common";
 import { SimpleComponent } from "./simple/simple.component";
 
+// ag-grid
+import { AgGridModule } from "ag-grid-angular";
+
 @NgModule({
-  imports: [CommonModule],
+  imports: [CommonModule, AgGridModule.withComponents([])],
   declarations: [SimpleComponent]
 })
 export class PagesModule {}
--- src/app/pages/simple/simple.component.ts
+++ src/app/pages/simple/simple.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from "@angular/core";
 import { HttpClient } from "@angular/common/http";
+import "ag-grid-enterprise";
+import "ag-grid-community/dist/styles/ag-grid.css";
+import "ag-grid-community/dist/styles/ag-theme-alpine.css";
 
 @Component({
   selector: "app-simple",
