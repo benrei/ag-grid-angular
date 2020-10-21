1caa12a66bb2a6da55811acee63343a42e4686aa
benrei <benrei.dev@gmail.com>
2020-10-21 20:43:34 +0200

    1 file modified

--- src/app/grid/actions/fit-columns.component.ts
+++ src/app/grid/actions/fit-columns.component.ts
@@ -3,7 +3,7 @@ import { Component, Input } from "@angular/core";
 @Component({
   selector: "app-fit-columns-action",
   template: `
-    <button *ngIf="resetFn" mat-icon-button (click)="fitColumnsFn()">
+    <button *ngIf="fitColumnsFn" mat-icon-button (click)="fitColumnsFn()">
       <mat-icon>view_column</mat-icon>
     </button>
   `
