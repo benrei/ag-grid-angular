2fda9c2d9faf6a3762aab3d3df62a59d3aae7202
benrei <benrei.dev@gmail.com>
2020-11-25 09:27:11 +0100

    1 file modified

--- src/app/pages/pages.module.ts
+++ src/app/pages/pages.module.ts
@@ -8,8 +8,7 @@ import { RowGroupComponent } from "./row-group/row-group.component";
 import { GridModule } from "../grid/grid.module";
 import { SizeColumnsToFitComponent } from "./size-columns-to-fit/size-columns-to-fit.component";
 import { TimeRegistrationComponent } from "./time-registration/time-registration.component";
-import { SpikeServerSideComponent } from "./spike-server-side/spike-server-side.component";
-import { ServerSideComponent } from './server-side/server-side.component';
+import { ServerSideComponent } from "./server-side/server-side.component";
 
 @NgModule({
   imports: [CommonModule, AgGridModule.withComponents([]), GridModule],
@@ -18,14 +17,14 @@ import { ServerSideComponent } from './server-side/server-side.component';
     RowGroupComponent,
     SizeColumnsToFitComponent,
     TimeRegistrationComponent,
-    SpikeServerSideComponent,
+    ,
     ServerSideComponent
   ],
   exports: [
     SimpleComponent,
     TimeRegistrationComponent,
     SizeColumnsToFitComponent,
-    SpikeServerSideComponent
+    ServerSideComponent
   ]
 })
 export class PagesModule {}
