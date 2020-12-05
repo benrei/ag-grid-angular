ebfcb83f2eb75e7cce35b440173c207a7505d240
benrei <benrei.dev@gmail.com>
2020-12-05 13:20:12 +0100

    1 file modified

--- src/app/toolbar/toolbar.module.ts
+++ src/app/toolbar/toolbar.module.ts
@@ -2,10 +2,9 @@ import { NgModule } from "@angular/core";
 import { CommonModule } from "@angular/common";
 import { ToolbarComponent } from "./toolbar.component";
 import { AllMaterialModules } from "../material.module";
-import { MenuModule } from "../menu/menu.module";
 
 @NgModule({
-  imports: [CommonModule, AllMaterialModules, MenuModule],
+  imports: [CommonModule, AllMaterialModules],
   declarations: [ToolbarComponent],
   exports: [ToolbarComponent]
 })
