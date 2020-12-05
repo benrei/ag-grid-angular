4ddc0ea2bf9bcd39bae2faaaa8acd72fe9179d0c
benrei <benrei.dev@gmail.com>
2020-12-05 13:45:08 +0100

    1 file modified

--- src/app/grid/grid.module.ts
+++ src/app/grid/grid.module.ts
@@ -2,13 +2,7 @@ import { NgModule } from "@angular/core";
 import { CommonModule } from "@angular/common";
 import { GridHeaderComponent } from "./grid-header/grid-header.component";
 import { AllMaterialModules } from "../material.module";
-import { AddActionComponent } from "./actions/add-action.component";
-import { EditActionComponent } from "./actions/edit-action.component";
-import { DeleteActionComponent } from "./actions/delete-action.component";
-import { RefreshActionComponent } from "./actions/refresh-action.component";
-import { ResetColumnsActionComponent } from "./actions/reset-columns-action.component";
 import { GridDirective } from "./grid.directive";
-import { FitColumnsActionComponent } from "./actions/fit-columns.component";
 import { PdfIconRenderer } from "./frameworkComponents/pdf-icon-renderer.component";
 import { SelectBoxEditor } from "./editors/select-box-editor/select-box-editor.component";
 import { DatepickerEditor } from "./editors/datepicker-editor/datepicker-editor.component";
@@ -29,12 +23,7 @@ import { ActionsModule } from "./actions/actions.module";
   ],
   exports: [
     GridHeaderComponent,
-    AddActionComponent,
-    EditActionComponent,
-    DeleteActionComponent,
-    RefreshActionComponent,
-    ResetColumnsActionComponent,
-    FitColumnsActionComponent,
+    ActionsModule,
     PdfIconRenderer,
     GridDirective,
     SelectBoxEditor,
