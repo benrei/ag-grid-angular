c4f92d285eafd7b7787da6aeb242ad23b9cf3f51
benrei <benrei.dev@gmail.com>
2020-12-10 16:58:49 +0100

    3 files modified

--- src/app/components/widgets/card-list/card-list.component.ts
+++ src/app/components/widgets/card-list/card-list.component.ts
@@ -1,21 +1,21 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from "@angular/core";
 
 @Component({
-  selector: 'app-card-list',
-  templateUrl: './card-list.component.html',
-  styleUrls: ['./card-list.component.css']
+  selector: "app-card-list",
+  templateUrl: "./card-list.component.html",
+  styleUrls: ["./card-list.component.css"]
 })
 export class CardListComponent implements OnInit {
   @Input() items;
-  constructor() { }
+  @Input() isRecursive = false;
+  constructor() {}
 
-  ngOnInit() {
-  }
+  ngOnInit() {}
   isArray(val) {
     return Array.isArray(val);
   }
   isObject(val) {
-    return typeof val === 'object' && val !== null;
+    return typeof val === "object" && val !== null;
   }
   printVal(val) {
     console.log(val);
