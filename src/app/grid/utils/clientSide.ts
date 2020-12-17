d7c32b60ad3bc46d714fde9c0a54e58db432d28c
benrei <benrei.dev@gmail.com>
2020-12-17 16:03:43 +0100

    1 file modified

--- src/app/grid/utils/clientSide.ts
+++ src/app/grid/utils/clientSide.ts
@@ -20,38 +20,4 @@ const client = {
   }
 };
 
-const getContextMenuItems = (params) => {
-  const { api, node } = params;
-  console.log(params);
-  const menuItems = [];
-  if (node) {
-    const { data, rowIndex } = node;
-    menuItems.push(
-      {
-        name: "Insert empty",
-        action: function () {
-          client.addRow(api, {}, rowIndex > 0 ? rowIndex - 1 : rowIndex);
-        },
-        tooltip:
-          "Very long tooltip, did I mention that I am very long, well I am! Long!  Very Long!"
-      },
-      {
-        name: "Duplicate",
-        action: function () {
-          client.addRow(api, JSON.parse(JSON.stringify(data)), rowIndex + 1);
-        },
-        tooltip:
-          "Very long tooltip, did I mention that I am very long, well I am! Long!  Very Long!"
-      },
-      "separator",
-      "copy",
-      "paste",
-      "separator",
-      "chartRange"
-    );
-  }
-  menuItems.push("export");
-  return menuItems;
-};
-
-export default { ...client, getContextMenuItems };
+export default { ...client };
