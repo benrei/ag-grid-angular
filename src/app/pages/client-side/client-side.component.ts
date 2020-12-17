97b463f805d227750ba19dc5d93a145cee5d2f5b
benrei <benrei.dev@gmail.com>
2020-12-17 16:12:11 +0100

    1 file modified, 1 deleted

--- src/app/pages/client-side/client-side.component.ts
+++ src/app/pages/client-side/client-side.component.ts
@@ -24,7 +24,7 @@ export class ClientSideComponent {
       selectBoxEditor: SelectBoxEditor,
       datepickerEditor: DatepickerEditor
     },
-    getContextMenuItems: contextMenu.standard,
+    getContextMenuItems: contextMenu.client,
     undoRedoCellEditing: true
     // editType: "fullRow"
   };
