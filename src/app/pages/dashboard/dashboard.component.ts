965630c5fef1245de8a67d858125f6bb5b328e0b
benrei <benrei.dev@gmail.com>
2021-01-11 15:06:14 +0100

    2 files modified

--- src/app/pages/dashboard/dashboard.component.ts
+++ src/app/pages/dashboard/dashboard.component.ts
@@ -29,6 +29,87 @@ export class DashboardComponent implements OnInit {
     ]
   ];
   budget = [{ field: "Budget" }];
+
+  widgetData = {
+    widgets: [
+      {
+        name: "Widget.Assignment.Spent",
+        data: {
+          items: [
+            {
+              name: "Service",
+              value: 11,
+              type: "Number"
+            },
+            {
+              name: "Cost",
+              value: 9241.55,
+              type: "Number",
+              group: true,
+              items: [
+                {
+                  name: "Service",
+                  value: 6809.44,
+                  type: "Number"
+                },
+                {
+                  name: "Products",
+                  value: 2432.11,
+                  type: "Number"
+                },
+                {
+                  name: "Other",
+                  value: 0,
+                  type: "Number",
+                  items: [
+                    {
+                      name: "Subcontractors",
+                      value: 0,
+                      type: "Number"
+                    }
+                  ]
+                }
+              ]
+            },
+            {
+              name: "Invoiced",
+              value: 5666.6,
+              type: "Number",
+              group: true,
+              items: [
+                {
+                  name: "Subinvoiced",
+                  value: 0,
+                  type: "Number"
+                }
+              ]
+            },
+            {
+              name: "BookedReserve",
+              value: 1200.3,
+              type: "Number"
+            },
+            {
+              name: "GrossMargin",
+              value: -3575.77,
+              type: "Number",
+              group: true
+            },
+            {
+              name: "PerHour",
+              value: -324.99,
+              type: "Number"
+            },
+            {
+              name: "Coverage",
+              value: 15,
+              type: "Percent"
+            }
+          ]
+        }
+      }
+    ]
+  };
   constructor() {}
 
   ngOnInit() {}
